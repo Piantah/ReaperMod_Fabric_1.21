78cbda0779bd504fd43fa5b35f7b6ed83da83959
Mr_Pianta <piantatsundere@gmail.com>
2025-06-08 01:40:57 +0200

    Aggiunti primi Item e Texture

---
+++ src/main/java/com/reapermod/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.reapermod.item;
+
+import com.reapermod.ReaperMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+public class ModItems {
+    private static Item registerItem( String name, Item item){
+    return Registry.register(Registries.ITEM, Identifier.of(ReaperMod.MOD_ID,name),item);
+    }
+    public static void registerModItems(){
+        ReaperMod.LOGGER.info("registrazione Item");
+
+        //tab ingredienti
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(Nether_Diamond);
+            entries.add(Raw_Uranium);
+        });
+    }
+
+    //Lista Item
+    public static final Item Nether_Diamond = registerItem("nether_diamond",new Item(new Item.Settings()));
+    public static final Item Raw_Uranium = registerItem("raw_uranium",new Item(new Item.Settings()));
+
+}
